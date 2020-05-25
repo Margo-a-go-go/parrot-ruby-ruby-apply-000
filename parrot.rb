9ba09757cdb964cc5114637c06e0f78488214137
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-05-25 01:52:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(pretty_bird = "Squawk!")
-  puts "#{pretty_bird}"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
   "Squawk!"
 end
